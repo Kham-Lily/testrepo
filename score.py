b8f3e22b94fe4de720ce427ef5fbc204226b120a
maingin <maingin189@gmail.com>
2024-02-26 14:09:52 +0800

    Add the score

--- score.py
+++ score.py
@@ -1,20 +1,30 @@
-"""
-CP1404/CP5632 - Practical
-Broken program to determine score status
-"""
+import random
 
 EXCELLENT_SCORE = 90
 PASS_SCORE = 50
+MINIMUM_SCORE = 0
+MAXIMUM_SCORE = 100
 
 
 def main():
-    """Get score from the users and display their status."""
+    score = get_valid_score()
+    result = determine_grade(score)
+    print(result)
+    random_score = random.uniform(MINIMUM_SCORE, MAXIMUM_SCORE)
+    random_score_result = determine_grade(random_score)
+    print(f"Random score: {random_score}")
+    print(random_score_result)
+
+
+def get_valid_score():
     score = get_score()
-    determine_status(score)
+    while score < MINIMUM_SCORE or score > MAXIMUM_SCORE:
+        print("Invalid Score!")
+        score = get_score()
+    return score
 
 
-def determine_status(score):
-    """Determine the status."""
+def determine_grade(score):
     if score >= EXCELLENT_SCORE:
         print("Excellent")
     elif score >= PASS_SCORE:
@@ -24,7 +34,6 @@ def determine_status(score):
 
 
 def get_score():
-    """Get score from the users."""
     score = float(input("Enter score: "))
     return score
 
